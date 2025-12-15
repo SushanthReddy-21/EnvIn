713358e5b362c2354df1001abfc469a96f65a228
SushanthReddy-21 <aletisushanthreddy@gmail.com>
2025-12-15 21:47:17 +0530

    enter

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class main{
+    public static void main(String[] args){
+        Scanner  sc=new Scanner(System.in);
+        System.out.println("Welcome to EnvIn");
+        System.out.println("Enter your name:");
+        String name=s.nextLine();
+        System.out.println("Hello, "+name+"!");
+        sc.close();
+    }
+}
